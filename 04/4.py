c3fe0d7d661d981523fe33dcd9063c089bf65f7d
Felix Hankel <hankel.felix@gmx.de>
2022-12-04 14:41:50 +0100

    Solved 4.2

--- 04/4.py
+++ 04/4.py
@@ -1,14 +1,20 @@
 class Worker:
     start = -1
     end = -1
-    def w_range(self): return self.end-self.start
+    w_range = -1
     def __init__(self, string):
         left, right = string.split("-")
         self.start = int(left)
         self.end = int(right)
+        self.w_range = range(self.start, self.end+1)
     def includes(self, worker2):
         if worker2.start >= self.start and worker2.end <= self.end:
             return True
+    def overlaps(self, worker2):
+        for i in range(self.start, self.end+1):
+            if i in worker2.w_range:
+                return True
+        return False
         
 
 def init():
@@ -28,6 +34,16 @@ def four1(data):
             x+=1
     print(f"1) Amount of fully including pairs: {x}")
     
+def four2(data):
+    x = 0
+    for entries in data:
+        left, right = entries.split(",")
+        w1 = Worker(left)
+        w2 = Worker(right)
+        if w1.overlaps(w2):
+            x+=1
+    print(f"2) Amount of overlapping pairs: {x}")
+    
 
 
 
@@ -36,3 +52,4 @@ def four1(data):
 
 data = init()
 four1(data)
+four2(data)
